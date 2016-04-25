8618c3b9b82587d038d7418b4768c17d0a506f45
Luiz Gustavo <luizgustavolino@gmail.com>
2016-04-25 19:51:42 -0300

    Adicionando classe Coordenada, em branco

---
+++ Bikes/Bikes/src/br/senac/pi/Coordenada.java
@@ -0,0 +1,19 @@
+
+package br.senac.pi;
+
+public class Coordenada {
+    
+    private double latitude;
+    private double longitude;
+
+    public Coordenada(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    static double distanciaEntre(Coordenada p1, Coordenada p2) {
+        // implementar
+        return 0.0;
+    }
+    
+}
