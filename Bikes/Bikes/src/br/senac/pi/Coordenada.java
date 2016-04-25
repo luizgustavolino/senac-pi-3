e01343f850416686d51bd91ddf1be06528da0306
Luiz Gustavo <luizgustavolino@gmail.com>
2016-04-25 20:32:47 -0300

    merge

--- Bikes/Bikes/src/br/senac/pi/Coordenada.java
+++ Bikes/Bikes/src/br/senac/pi/Coordenada.java
@@ -13,7 +13,7 @@ public class Coordenada {
         this.longitude = longitude;
     }
     
-    static double distanciaEntre(Coordenada p1, Coordenada p2) {
+    public static double distanciaEntre(Coordenada p1, Coordenada p2) {
         Double distancia = sqrt((Math.pow(p1.longitude, 2) - Math.pow(p1.latitude, 2))+(Math.pow(p2.longitude, 2) - Math.pow(p2.latitude, 2)));
         return distancia;
     }
