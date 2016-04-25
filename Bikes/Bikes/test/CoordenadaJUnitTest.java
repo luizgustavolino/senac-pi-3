3c114ecb2404c5f3a8e71ee764a16992914554f0
Luiz Gustavo <luizgustavolino@gmail.com>
2016-04-25 20:09:58 -0300

    Adicionando Teste Unitário

---
+++ Bikes/Bikes/test/CoordenadaJUnitTest.java
@@ -0,0 +1,37 @@
+
+import br.senac.pi.Coordenada;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CoordenadaJUnitTest {
+    
+    public CoordenadaJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testarDistancia() {
+        Coordenada c1 = new Coordenada(15.0, 11.0);
+        Coordenada c2 = new Coordenada(3.0, 20.0);
+        assertEquals(15.0, Coordenada.distanciaEntre(c1, c2), 0.0);
+    }
+}
