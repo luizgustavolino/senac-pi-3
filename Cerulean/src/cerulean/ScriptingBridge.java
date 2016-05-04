3f2b1d6a8e3dcf1166bd0b0e41a24ba4fde35219
Lino <luizgustavolino@gmail.com>
2016-05-04 19:06:21 -0300

    Adicionando css via java

--- Cerulean/src/cerulean/ScriptingBridge.java
+++ Cerulean/src/cerulean/ScriptingBridge.java
@@ -3,13 +3,19 @@ package cerulean;
 import java.net.URL;
 
 public class ScriptingBridge {
-    public String getJARPath(){
+    
+    public String getJARPath(String filename){
         URL base = Cerulean.class.getProtectionDomain().getCodeSource().getLocation();
-        return base.toString();
+        String path = "jar:" + base.toString() + "!/res/html/" + filename;
+        return path;
     }
     
     public void log(String message){
         System.out.println("[Cerulean] " + message);
+    }
+    
+    public Boolean isNative(){
+        return true;
     }
             
 }
